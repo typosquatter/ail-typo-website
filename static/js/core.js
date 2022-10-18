d1d2c38d11b1d4efa52affd89a867c59ef6ae883
David Cruciani <da.cruciani@laposte.net>
2022-10-18 18:23:49 +0200

    add: [functionnality] sorted column

--- static/js/core.js
+++ static/js/core.js
@@ -5,10 +5,10 @@ function fetchDomains() {
     $.getJSON('/domains/' + $('#sid').val(), function(data) {
         $('#data').empty();
         $('<tr>').append(
-            $('<th>').text("PERMUTATION"),
-            $('<th>').text("IP ADDRESS"),
-            $('<th>').text("NAME SERVER"),
-            $('<th>').text("MAIL SERVER"),
+            $('<th>').text("PERMUTATION").click(function(){ sort_table(0) }),
+            $('<th>').text("IP ADDRESS").click(function(){ sort_table(1) }),
+            $('<th>').text("NAME SERVER").click(function(){ sort_table(2) }),
+            $('<th>').text("MAIL SERVER").click(function(){ sort_table(3) }),
         ).appendTo('#data')
         $('<tr>').attr("id", "last_line").appendTo("#data")
 
@@ -364,3 +364,84 @@ function checkShare(){
 }
 
 
+
+function sort_table(column){
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("data");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc"; 
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        if( i + 1< (rows.length - 1)){
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[column];
+            y = rows[i + 1].getElementsByTagName("TD")[column];
+
+            if(column==1){
+                x_split = x.innerHTML.toLowerCase().split(".")[0]
+                y_split = y.innerHTML.toLowerCase().split(".")[0]
+            }
+            if(column == 3){
+                x_split = x.innerHTML.toLowerCase().split(" ")[1]
+                y_split = y.innerHTML.toLowerCase().split(" ")[1]
+            }
+            if(column == 3 || column == 1){
+                if (dir == "asc") {
+                    if (x_split > y_split) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch= true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (x_split < y_split) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            else{
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch= true;
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        //Each time a switch is done, increase this count by 1:
+        switchcount ++;      
+      } else {
+        /*If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again.*/
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+}
